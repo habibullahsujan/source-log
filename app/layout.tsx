ccd2e0a5794d543bb099a28788576a1eba56e9d6
habibullahsujan <habibsujan007@gmail.com>
2024-02-26 19:51:28 +0300

    done

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Noto_Serif_Georgian, Manrope } from 'next/font/google'
 import clsx from "clsx";
 import { Toaster } from "@/components/ui/toaster";
+import { GoogleAnalytics } from '@next/third-parties/google'
 export const metadata: Metadata = {
   title: "Blog Source",
   description: "Arabic Lifestyles, their calture and their favorite food.",
@@ -23,6 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={clsx('font-mr bg-light dark:bg-dark', notoSerifGeorgian.variable, manrope.variable)}>{children}   <Toaster /></body>
+      <GoogleAnalytics gaId="G-EEQ48444N2" />
     </html>
   );
 }
