336e88164b0ae9a630b6fb621cf8c0cf13318055
habibullahsujan <habibsujan007@gmail.com>
2024-05-06 21:22:01 +0300

    done

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 
+
+
+
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -13,6 +16,29 @@ const nextConfig = {
       }
     ],
   },
+
 };
+
+
+export async function redirects() {
+  return [
+    {
+      source: 'http://www.theblogsouce.com/',
+      destination: 'https://www.theblogsouce.com/',
+      permanent: true,
+    },
+    {
+      source: 'http://theblogsouce.com/',
+      destination: 'https://www.theblogsouce.com/',
+      permanent: true,
+    },
+    {
+      source: 'https://theblogsouce.com/',
+      destination: 'https://www.theblogsouce.com/',
+      permanent: true,
+    },
+
+  ];
+}
 
 export default nextConfig
